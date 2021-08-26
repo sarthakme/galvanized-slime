f8b29b5befd912f6278de46c58ee278e935ae21c
sarthakme <sarthakrocks99@yahoo.com>
2021-08-27 00:16:22 +0530

    Added .ebignore

--- Backend/res/post.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-const file = '.env';
-
-var fileStream = fs.readFileSync(file, 'utf-8');
-var newfs = fileStream.replace('ENV=test', 'ENV=dev');
-fs.writeFileSync(file, newfs);
